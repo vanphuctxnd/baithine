df69ef075de72136979453c18ff7d63edf44c96f
vanphuctxnd <112148433+vanphuctxnd@users.noreply.github.com>
2023-05-31 09:58:42 +0700

    Add files via upload

---
+++ bai_tap_thi/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai_tap_thi
+{
+        class Cylinder
+        {
+            private double radius;
+            private double height;
+            private double baseArea;
+            private double lateralArea;
+            private double totalArea;
+            private double volume;
+
+            public void Process()
+            {
+            Console.WriteLine("Enter the dimensions of the cylinder");
+            Console.Write("Radius : ");
+                radius = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Height : ");
+                height = Convert.ToDouble(Console.ReadLine());
+
+            baseArea = Math.Round(Math.PI * radius * radius, 2);
+            lateralArea = Math.Round(2 * Math.PI * radius * height, 2);
+            totalArea = Math.Round(2 * Math.PI * radius * (height + radius), 2);
+            volume = Math.Round(Math.PI * radius * radius * height, 2);
+        }
+
+            public string Result()
+            {
+                return $" \nBase: { baseArea} Latera : { lateralArea} Total : { totalArea} Volume :  {volume}";
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Cylinder cylinder = new Cylinder();
+                cylinder.Process();
+
+                Console.WriteLine("\nCylinder Characteristic:");
+                Console.WriteLine(cylinder.Result());
+
+                Console.ReadLine();
+            }
+        }
+}
---
+++ bai_thi_nè_2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai_thi_nè_2
+{
+    class Animal
+    {
+        protected double weight;
+        protected string name;
+
+        public void SetMe(double weight, string name)
+        {
+            this.weight = weight;
+            this.name = name;
+        }
+
+        public virtual void Show()
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Weight: {weight} kg");
+        }
+    }
+
+    class tiger : Animal
+    {
+        public tiger(double weight, string name)
+        {
+            base.SetMe(weight, name);
+        }
+    }
+
+    class lion : Animal
+    {
+        public lion(double weight, string name)
+        {
+            base.SetMe(weight, name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tiger Tiger = new tiger(200, "Tiger");
+            lion Lion = new lion(180, "Lion");
+
+            Tiger.Show();
+            Console.WriteLine();S
+
+            Lion.Show();
+            Console.WriteLine();
+        }
+    }
+}
